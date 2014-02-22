b4e4c24421a7946fa297c5178ddd39624c833985
wangjoshuah <joshua.h.wang.15@dartmouth.edu>
2014-02-22 16:05:28 -0500

    set up classes
    give prom to alex

--- src/Actor.java
+++ src/Actor.java
@@ -13,10 +13,6 @@ public class Actor {
 	{
 		name = person;
 		movies = new ArrayList<Movie>();
-		number = Integer.MAX_VALUE;
-		myCoStars = null;
-		prevActor = null;
-		sharedMovie = null;
 	}
 	public String getName() {
 		return name;
@@ -26,50 +22,12 @@ public class Actor {
 		return movies;
 	}
 
-	public HashMap<Actor, Movie> coStars() {
-		if (myCoStars != null)
-			return myCoStars;
-		myCoStars = new HashMap<Actor,Movie>();
-		for (Movie m: movies)
-		{
-			for (Actor a: m.getActors())
-			{
-				if (a != this)
-				{
-					myCoStars.put(a, m);
-				}
-			}
-		}
-		return myCoStars;
-	}
 	public void add(Movie m)
 	{
 		movies.add(m);
 	}
-	public int getNumber() {
-		return number;
-	}
-	public void setNumber(int n) {
-		if (n < number)
-			number = n;
-		
-	}
-	public Actor getPrevActor() {
-		return prevActor;
-	}
-	public void setPrevActor(Actor prevActor) {
-		this.prevActor = prevActor;
-	}
-	public int hashCode()
-	{
-		return name.hashCode();
-	}
-	public Movie getSharedMovie() {
-		return sharedMovie;
-	}
-	public void setSharedMovie(Movie sharedMovie) {
-		this.sharedMovie = sharedMovie;
-	}
+	
+	
 	
 	
 }
