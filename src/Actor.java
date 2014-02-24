27c985cdc425fb2dbc2e24d04c957d1aaf30014d
wangjoshuah <joshua.h.wang.15@dartmouth.edu>
2014-02-23 22:00:13 -0500

    create Kevin Tree
    can create the tree now but need to print it

--- src/Actor.java
+++ src/Actor.java
@@ -4,12 +4,22 @@ public class Actor {
 	private String name;
 	private Set<Movie> movies;
 	private Set<Actor> adjacentActors;
+	private Actor prevActor;
+	private Actor baconLink;
+	private int baconNumber;
+	private Map<Actor, Movie> movieLinks;
+	private Movie baconMovie;
 	
 	public Actor(String person)
 	{
 		name = person;
 		movies = new HashSet<Movie>();
+		prevActor = null;
+		baconLink = null;
+		movieLinks = new HashMap<Actor, Movie>();
+		baconMovie = null;
 	}
+	
 	public String getName() {
 		return name;
 	}
@@ -17,12 +27,41 @@ public class Actor {
 	public Set<Movie> getMovies() {
 		return movies;
 	}
+	
+	public Actor getPrevActor() {
+		return prevActor;
+	}
+	
+	public Actor getBaconLink() {
+		return baconLink;
+	}
+	
+	public int getBaconNumber() {
+		return baconNumber;
+	}
+	
+	public Movie getBaconMovie() {
+		return baconMovie;
+	}
 
 	public void add(Movie m)
 	{
 		movies.add(m);
 	}
 	
+	public void setPrevActor(Actor path) {
+		prevActor = path; //set our previous actor from the path we are coming from
+	}
+	
+	public void setBaconLink(Actor path) {
+		baconLink = path; //set the bacon link
+		baconMovie = movieLinks.get(baconLink); //set our bacon movie based on our bacon link
+	}
+	
+	public void setBaconNumber(int distance) {
+		baconNumber = distance;
+	}
+		
 	public Set<Actor> getAdjacentActors() {
 		if (adjacentActors == null) { //if we don't have a set yet
 			adjacentActors = new HashSet<Actor>(); //instantiate the set
@@ -39,9 +78,4 @@ public class Actor {
 		}
 		return adjacentActors; //return our list of adjacent actors
 	}
-	
-	
-	
-	
-	
 }
--- src/BaconGraph.java
+++ src/BaconGraph.java
@@ -17,14 +17,55 @@ public class BaconGraph {
 		actorIDs = new HashMap<String, Actor>();
 		movieIDs = new HashMap<String, Movie>();
 	}
-	
-	
-	
-	public void getBaconNumber(String name) {
-		
 
+	public void getBaconNumber(String name) {
+		int baconNumber = 0; //set our bacon Number to be 0
+		if (name.equals("Kevin Bacon")) { //if we are looking for Kevin Bacon (trivial case)
+			System.out.println("Kevin Bacon's Bacon Number is 0."); //print our distance
+		}
+		else { //non trivial case
+			Actor source = searchByName(name); //get the source actor
+			Queue<Actor> searchQueue = new LinkedList<Actor>(); //queue of actors we need to search
+			Set<Actor> visitedActors = new HashSet<Actor>(); //keep track of where we have been
+			while (!searchQueue.isEmpty()) { //while the search queue isn't empty
+				baconNumber ++; //increment our bacon number
+				Actor query = searchQueue.poll(); //get the next actor we should look at in our queue
+
+				Set<Actor> adjacencies = query.getAdjacentActors(); //get the adjacent actors to our source
+				visitedActors.add(query); //add our source actor to the list of actors we have visited
+				for (Actor adjacency : adjacencies) { // for all the adjacent actors,
+					if (!visitedActors.contains(adjacency)) { //if we haven't visited the query yet
+						adjacency.setPrevActor(query); //set the adjacent actors' path
+						searchQueue.add(adjacency); //add each adjacency to the queue we need to search
+					}
+				}			
+			}
+
+		}
 	}
 
+	public void createBaconGraph() {
+		Actor source  = searchByName("Kevin Bacon"); //get Kevin Bacon
+		source.setBaconNumber(0); //set Kevin Bacon's bacon number to be 0
+		source.setBaconLink(source); //Kevin Bacon is his own link
+		
+		Queue<Actor> searchQueue = new LinkedList<Actor>(); //queue of actors we need to search
+		searchQueue.add(source); //add kevin bacon in
+		Set<Actor> visitedActors = new HashSet<Actor>(); //create a list of actors we've visited
+		while (!searchQueue.isEmpty()) { //until the search queue is empty
+			Actor query = searchQueue.poll(); //get the next thing we need to look at
+			visitedActors.add(query); //add it to our list of visited actors
+			Set<Actor> adjacencies = query.getAdjacentActors(); //get the adjacencies
+			for (Actor adjacency: adjacencies) { //for all adjacent actors
+				if (!visitedActors.contains(adjacency)) { //if we haven't looked at them yet
+					searchQueue.add(adjacency); //add them to our search queue
+					adjacency.setBaconNumber(query.getBaconNumber() + 1); //set their bacon number
+					adjacency.setBaconLink(query); //as well as their bacon path and movie
+				}
+			}
+		}
+	}
+	
 	/**
 	 * Add an actor to our map of actors
 	 * @param idNumber 	use the id number from the data
@@ -35,7 +76,7 @@ public class BaconGraph {
 		actorNames.put(name, idNumber); //put the actor's name in a map getting the id value
 		actorIDs.put(idNumber, actor); //get the actor from their id value
 	}
-	
+
 	/**
 	 * searches for an actor by their name instead of the id number
 	 * @param name		name of the actor
@@ -61,23 +102,23 @@ public class BaconGraph {
 	 */
 	public void testGraphCreator() throws Exception{
 		//create our test Graph		
-		
+
 		//input actors text
 		BufferedReader actorInput = new BufferedReader(new FileReader("inputs/actorsTest.txt"));
 		String line = new String();
 		String[] parts = new String[2];
 		while ((line = actorInput.readLine()) != null) {
-		    parts = line.split("\\|");
-		    addActor(parts[0], parts[1]);
+			parts = line.split("\\|");
+			addActor(parts[0], parts[1]);
 		}
-		
+
 		//input movies
 		BufferedReader movieInput = new BufferedReader(new FileReader("inputs/moviesTest.txt"));
 		while ((line = movieInput.readLine()) != null) {
-		    parts = line.split("\\|");
-		    addMovie(parts[0], parts[1]);
+			parts = line.split("\\|");
+			addMovie(parts[0], parts[1]);
 		}
-		
+
 		//get their relations
 		BufferedReader relationsInput = new BufferedReader(new FileReader("inputs/movie-actorsTest.txt"));
 		while ((line = relationsInput.readLine()) != null) {
@@ -91,6 +132,11 @@ public class BaconGraph {
 		System.out.println("end");
 	}
 	
+	public void getBaconInfo(String name) { //print out the bacon info for a star
+		Actor star = searchByName(name); //get the star
+		System.out.println();
+	}
+
 	/**
 	 * @param args
 	 */
@@ -103,10 +149,12 @@ public class BaconGraph {
 			e.printStackTrace();
 		}
 		
+		testGraph.createBaconGraph(); //set the bacon info for each actor
+
 		String command = "";
 		Scanner input = new Scanner(System.in);
 		System.out.println("To quit the program, type return in answer to a question.");
-		
+
 		while (true) {
 			System.out.println("Enter the name of an actor:");
 			command = input.nextLine();
